ec64aea0c03e9b6d0d5ac5aef16b4167d7be654b
dev-donghwan <ycdoit@naver.com>
2024-08-24 21:20:42 +0900

    [#6] feat: 이메일 전송 Component 작성 및 테스트코드 작성

---
+++ src/main/java/com/donghwan/javaboard/email/EmailSender.java
@@ -0,0 +1,25 @@
+package com.donghwan.javaboard.email;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class EmailSender {
+
+    private final JavaMailSender mailSender;
+
+    public void send(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+        log.info("[EmailSender] success");
+    }
+}
